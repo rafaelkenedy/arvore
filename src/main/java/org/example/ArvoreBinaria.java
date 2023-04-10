565a725a55a0b686c84c74f231bb3e38dbdb0e2e
rafael <k.rafaelalves@gmail.com>
2023-04-09 23:01:49 -0300

    método inserir

---
+++ src/main/java/org/example/ArvoreBinaria.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria() {
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo) {
+        BinNo<T> novoNo = new BinNo<>(conteudo);
+        raiz = inserir(raiz, novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+        if (atual == null) {
+            return novoNo;
+        } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }
+        return atual;
+    }
+}
